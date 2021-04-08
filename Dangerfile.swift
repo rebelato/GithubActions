ff06a558e65783b4fbc932e1739e00d4417cdd86
Lucas Rivellino Rebelato <lucas.r.rebelato@accenture.com>
2021-04-08 20:35:14 -0300

    update ci

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,11 +1,5 @@
 import Danger
 
 let danger = Danger()
-let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
-
-let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
-
-if !changelogChanged && sourceChanges != nil {
-  warn("No CHANGELOG entry added.")
-}
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+message("These files have changed: \(editedFiles.joined(separator: ", "))")
