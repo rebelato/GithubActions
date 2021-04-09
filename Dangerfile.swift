59e832bef8de518ffcd0a655b431634fbb64b996
Lucas Rivellino Rebelato <lucas.r.rebelato@accenture.com>
2021-04-09 17:00:14 -0300

    update ci

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,6 +5,19 @@ let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
 
+//danger.github.pullRequest.title
+
 if !changelogChanged {
   fail("No CHANGELOG entry added.")
+}
+
+let titleMR = danger.github.pullRequest.title
+if titleMR.isEmpty {
+    fail("Titulo do mr vazio.")
+}
+
+if let commitCount = danger.github.pullRequest.commitCount {
+    if commitCount > 10 {
+        fail("limite de commit excedido.")
+    }
 }
