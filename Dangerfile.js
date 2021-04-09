f07e2c303c51e1273386ed5a9ee7bffe9989084f
Lucas Rivellino Rebelato <lucas.r.rebelato@accenture.com>
2021-04-09 16:35:42 -0300

    update ci

--- Dangerfile.js
+++
@@ -1,6 +0,0 @@
-const hasChangelog = danger.git.modified_files.includes("changelog.md")
-const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes("#trivial")
-
-if (!hasChangelog && !isTrivial) then
-  warn("Please add a changelog entry for your changes.")
-end
