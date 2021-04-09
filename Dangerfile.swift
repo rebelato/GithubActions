6e0a3bf2a9ce57f29b4bd35b9e8e9b7247335b73
Lucas Rivellino Rebelato <lucas.r.rebelato@accenture.com>
2021-04-09 17:29:11 -0300

    update ci

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,10 +5,8 @@ let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
 
-//danger.github.pullRequest.title
-
 if !changelogChanged {
-  fail("No CHANGELOG entry added.")
+  fail("Não foi adicionado CHANGELOG")
 }
 
 let pattern = "\\[+([A-Z-a-z])+\\/(CB)+-+\\d{4}\\]+ -"
@@ -18,5 +16,11 @@ let result = titleMR.range(of: pattern, options:.regularExpression)
 
 if result == nil {
     fail("Titulo do mr nao esta no padrao")
-    message("O padrão deve ser [TipoTask/CB-NumeroTask] - Descrição")
+    message("O padrão de titulo de MR deve ser [TipoTask/CB-NumeroTask] - Descrição")
+}
+
+if let commitCount = danger.github.pullRequest.commitCount {
+    if commitCount > 10 {
+        fail("limite de commit excedido.")
+    }
 }
